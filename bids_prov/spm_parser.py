c2b75c8a61e31f7df18248de08edaaf027aa6133
cyril-data <cyrilregan@mail.com>
2022-11-22 08:33:46 +0100

    vscode_autopep8 test on bids_prov/spm_parser.py

--- bids_prov/spm_parser.py
+++ bids_prov/spm_parser.py
@@ -11,7 +11,9 @@ from . import spm_load_config as conf
 from . import get_id
 
 
-def format_activity_name(s, l=30):  # s example : cfg_basicio.file_dir.file_ops.file_move._1
+def format_activity_name(
+    s, l=30
+):  # s example : cfg_basicio.file_dir.file_ops.file_move._1
     if s.startswith("spm."):
         s = s[4:]
     tmp = s.split(".")  # ['cfg_basicio', 'file_dir', 'file_ops', 'file_move', '_1']
@@ -42,23 +44,29 @@ def get_input_entity(left, right):
         # if not None (if it doesn't match with conf.PATH_REGEX), enter in if
         print("the string does not match with conf.PATH_REGEX")
         return None
-    if next(re.finditer(conf.FILE_REGEX, right), None) is None:  # r"(\.[a-z]{1,3}){1,2}"
+    if (
+        next(re.finditer(conf.FILE_REGEX, right), None) is None
+    ):  # r"(\.[a-z]{1,3}){1,2}"
         # the string does not contain a filename extension so this is not an entity
         print("the string does not contain a filename so this is not an input_entity")
         return None
 
-    entity_label = re.sub(r"[{};\'\"]", "", right).split("/")[-1]  # sub allows you to remove braces; apostrophe and
+    entity_label = re.sub(r"[{};\'\"]", "", right).split("/")[
+        -1
+    ]  # sub allows you to remove braces; apostrophe and
     # quotation mark.
     # If we have : "$HOME/nidmresults-examples/spm_default/ds011/sub-01/func/sub-01_task-tonecounting_bold.nii.gz",
     # the line will return "sub-01_task-tonecounting_bold.nii.gz" and not "sub-01_task-tonecounting_bold.nii.gz'};"
-    print(f'entity label : {entity_label}')
+    print(f"entity label : {entity_label}")
     entity = {
         "@id": "niiri:" + entity_label + get_id(),
         "label": entity_label,
-        "prov:atLocation": right[2:-3],  # similar processing with respect to the entity_label variable. The line
+        "prov:atLocation": right[
+            2:-3
+        ],  # similar processing with respect to the entity_label variable. The line
         # removes "{'" at the beginning and "'};" at the end
     }
-    print(f'entity : {entity}')
+    print(f"entity : {entity}")
     return entity
 
 
@@ -112,14 +120,20 @@ def group_lines(lines):
         a = re.search(r"\{\d+\}", line)
         if a:
             g = a.group()[1:-1]  # retrieves the batch number without the braces
-            res[g].append(line[a.end() + 1:])  # retrieves the rest of the line without the dot after the brace of
+            res[g].append(
+                line[a.end() + 1 :]
+            )  # retrieves the rest of the line without the dot after the brace of
             # the activity number
 
-    new_res = dict()  # keys : common prefix shared by the functions of an activity, values : rest of each line
+    new_res = (
+        dict()
+    )  # keys : common prefix shared by the functions of an activity, values : rest of each line
     for k, v in res.items():
         common_prefix = os.path.commonprefix([_.split(" = ")[0] for _ in v])
         new_key = f"{common_prefix}_{k}"  # add to the common prefix the activity number
-        new_res[new_key] = [_[len(common_prefix):] for _ in v]  # keep the rest of the line
+        new_res[new_key] = [
+            _[len(common_prefix) :] for _ in v
+        ]  # keep the rest of the line
     return new_res
 
 
@@ -134,7 +148,7 @@ def get_records(task_groups: dict, records=defaultdict(list)):
     entities_ids = set()
     print(f"task_groups : {task_groups}")
     for activity_name, values in task_groups.items():
-        print('-'*50)
+        print("-" * 50)
         activity_id = "niiri:" + activity_name + get_id()
         activity = {
             "@id": activity_id,
@@ -164,7 +178,9 @@ def get_records(task_groups: dict, records=defaultdict(list)):
                 )
 
         for line in values:
-            split = line.split(" = ")  # split in 2 at the level of the equal the rest of the action
+            split = line.split(
+                " = "
+            )  # split in 2 at the level of the equal the rest of the action
             if len(split) != 2:
                 print(f"could not parse {line}")
                 continue
@@ -173,15 +189,25 @@ def get_records(task_groups: dict, records=defaultdict(list)):
             _in = get_input_entity(left, right)
             if _in:
                 input_entities.append(_in)
-            elif (conf.has_parameter(left) or conf.has_parameter(activity_name)) and ("substruct" in left or "substruct" in activity_name or "substruct" in right):
+            elif (conf.has_parameter(left) or conf.has_parameter(activity_name)) and (
+                "substruct" in left
+                or "substruct" in activity_name
+                or "substruct" in right
+            ):
                 print("elif")
                 # or has_parameter(activity_name) is mandatory because if in our activity we have only one call
                 # to a function, the common part will be full and so left will be empty
-                dependency = re.search(conf.DEPENDENCY_REGEX, right, re.IGNORECASE)  # cfg_dep\(['"]([^'"]*)['"]\,.*
+                dependency = re.search(
+                    conf.DEPENDENCY_REGEX, right, re.IGNORECASE
+                )  # cfg_dep\(['"]([^'"]*)['"]\,.*
                 # check if the line call cfg_dep and retrieve the first parameter
-                dep_number = re.search(r"{(\d+)}", right)  # retrieve all digits between parenthesis
+                dep_number = re.search(
+                    r"{(\d+)}", right
+                )  # retrieve all digits between parenthesis
                 if dependency is not None:
-                    parts = dependency.group(1).split(": ")  # retrieve name of the output_entity
+                    parts = dependency.group(1).split(
+                        ": "
+                    )  # retrieve name of the output_entity
                     # if right = "cfg_dep('Move/Delete Files: Moved/Copied Files', substruct('.',...));"
                     # return : ['Move/Delete Files', 'Moved/Copied Files']
                     closest_activity = next(
@@ -196,9 +222,13 @@ def get_records(task_groups: dict, records=defaultdict(list)):
                     print(f"closest_activity : {closest_activity}")
                     if closest_activity is None:
                         continue
-                    output_id = "niiri:" + parts[-1].replace(" ", "") + dep_number.group(1)
+                    output_id = (
+                        "niiri:" + parts[-1].replace(" ", "") + dep_number.group(1)
+                    )
                     # example : "niiri:oved/CopiedFiles1
-                    activity["used"].append(output_id)  # adds to the current activity the fact that it has used the
+                    activity["used"].append(
+                        output_id
+                    )  # adds to the current activity the fact that it has used the
                     # previous entity
                     output_entities.append(
                         {
@@ -213,8 +243,12 @@ def get_records(task_groups: dict, records=defaultdict(list)):
                     Warning(f"Could not parse line {line}")
             else:
                 print("params")
-                param_name = ".".join(left.split(".")[-2:])  # split left by "." and keep the two last elements
-                param_value = preproc_param_value(right[:-1])  # remove ";" at the end of right
+                param_name = ".".join(
+                    left.split(".")[-2:]
+                )  # split left by "." and keep the two last elements
+                param_value = preproc_param_value(
+                    right[:-1]
+                )  # remove ";" at the end of right
                 print(param_name, param_value)
                 # HANDLE STRUCTS eg. struct('name', {}, 'onset', {}, 'duration', {})
                 # if param_value.startswith("struct"):
@@ -232,7 +266,9 @@ def get_records(task_groups: dict, records=defaultdict(list)):
         if input_entities:
             used_entities = [e["@id"] for e in input_entities]
             print(f'activity["used"] : {activity["used"]}')
-            activity["used"] = activity["used"] + used_entities  # we add entities from input_entities
+            activity["used"] = (
+                activity["used"] + used_entities
+            )  # we add entities from input_entities
         entities = input_entities + output_entities
         if params:
             activity["attributes"] = params
