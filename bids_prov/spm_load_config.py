31b0a7bc7173d13e7aca83df0f8a8425a07312bb
hermann74 <hermann.courteille@irisa.fr>
2022-12-13 14:53:19 +0100

    Fix somes points from PR reviews

--- bids_prov/spm_load_config.py
+++ bids_prov/spm_load_config.py
@@ -1,34 +1,42 @@
 import re
 import yaml
 import os
+import hashlib
 
 # contains the path from home to the directory where this script is located
 this_path = os.path.dirname(os.path.abspath(__file__))
 
 PATH_REGEX = r"([A-Za-z]:|[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*)((/[A-Za-z0-9_.-]+)+)"
-PARAM_REGEX = r"[^\.]+\(\d+\)"  # balbla.fonc(53)
+PARAM_REGEX = r"[^\.]+\(\d+\)"  # example: some_activity.function(53)
 FILE_REGEX = r"(\.[a-z]{1,3}){1,2}"  #the string does not contain a filename so this is not an input_entity
 DEPENDENCY_REGEX = r"""cfg_dep\(['"]([^'"]*)['"]\,.*"""  # TODO : add ": " in match
 
-has_parameter = lambda line: re.search(PARAM_REGEX, line) is not None
 # has_entity = lambda line: not has_parameter(line) and next(re.finditer(PATH_REGEX, line), None) is not None
-# a string contains at least one parameter if  it does not start with a dot and contains at least one digit between
-# brackets. if there are parameters, they are necessarily in the left part (function call) and this is not an entity
+has_parameter = lambda line: re.search(PARAM_REGEX, line) is not None
+
 CONTEXT_URL = "https://raw.githubusercontent.com/bids-standard/BEP028_BIDSprov/master/context.json"
 
 with open(this_path + "/spm_config.yml", "r") as fd:
     static = yaml.load(fd, Loader=yaml.CLoader)
 
 
-def get_empty_graph(context_url=CONTEXT_URL):
+def get_sha256(file_path):
+    m = hashlib.sha256()
+    with open(file_path,'rb') as f:
+        lines = f.read()
+        m.update(lines)
+    md5code = m.hexdigest()
+    return md5code
+
+
+def get_empty_graph(context_url=CONTEXT_URL,spm_ver="dev"):
     return {
         "@context": context_url,
-        "BIDSProvVersion": "1.0.0",  # TODO ?
+        "BIDSProvVersion": "dev",  # TODO ?
         "@id": "http://example.org/ds00000X",  # TODO ?
         "wasGeneratedBy": {
             "@id": "INRIA",
             "@type": "Project",
-            "startedAt": "2016-09-01T10:00:00",  # TODO ?
             "wasAssociatedWith": {
                 "@id": "NIH",
                 "@type": "Organization",
@@ -42,7 +50,7 @@ def get_empty_graph(context_url=CONTEXT_URL):
                     "RRID": "RRID:SCR_007037",
                     "@type": "prov:SoftwareAgent",
                     "label": "SPM",
-                    "version": "dev"
+                    "version": spm_ver
                 }
             ],
             "prov:Activity": [],
