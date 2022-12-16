edc26686932d78cdceb91074530850679a6c6bc3
cyril-data <cyrilregan@mail.com>
2022-12-16 09:37:33 +0100

    random seed =14 only for testing

--- bids_prov/__init__.py
+++ bids_prov/__init__.py
@@ -1,15 +1,10 @@
 import string
 import random
-## TODO uuid from special library (20 alpha num)
+# TODO uuid from special library (20 alpha num)
 # https://stackoverflow.com/questions/41186818/how-to-generate-a-random-uuid-which-is-reproducible-with-a-seed-in-python
 
-random.seed(14) # Control random generation for test, init at each import
-INIT_STATE = random.getstate()
-
-
-def init_random_state(): # force init to initial state
-    random.setstate(INIT_STATE)
-
 # generates a string containing 10 letters (upper or lower case, 52 possible characters)
+
+
 def get_id():
     return "".join(random.choice(string.ascii_letters + string.digits) for i in range(20))
