4da8ad60403a35b03488de8c9f2212dfe96078cc
thomasbtnfr <thomas.betton@irisa.fr>
2023-01-18 10:34:47 +0100

    fix CONTEXT_URL

--- bids_prov/spm_load_config.py
+++ bids_prov/spm_load_config.py
@@ -11,7 +11,11 @@ PARAM_REGEX = r"[^\.]+\(\d+\)"  # example: some_activity.function(53)
 FILE_REGEX = r"(\.[a-z]{1,3}){1,2}"  # the string does not contain a filename so this is not an input_entity
 DEPENDENCY_REGEX = r"""cfg_dep\(['"]([^'"]*)['"]\,.*"""  # TODO : add ": " in match
 
-has_parameter = lambda line: re.search(PARAM_REGEX, line) is not None
+
+def has_parameter(line):
+    return re.search(PARAM_REGEX, line) is not None
+
+
 # has_entity = lambda line: not has_parameter(line) and next(re.finditer(PATH_REGEX, line), None) is not None
 
 with open(this_path + "/spm_config.yml", "r") as fd:
