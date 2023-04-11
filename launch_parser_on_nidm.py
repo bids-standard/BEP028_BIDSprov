848c2835bb650d4e594cb79a22377d52fecdb1f1
cyril-data <cyrilregan@mail.com>
2023-04-11 16:19:20 +0200

    add visualization on afni bloc

--- launch_parser_on_nidm.py
+++ launch_parser_on_nidm.py
@@ -15,17 +15,20 @@ def process_file(context_write, root, file, filename_ss_ext, output_dir, parser_
     """Process a file using the given parser function and save the output to the output directory."""
     context_write.write(f"    file= {root}/{str(file)}\n")
     print(parser_function)
-
     filename = root + "/" + str(file)
     shutil.copyfile(filename, output_dir + "/" + str(file))
     output_jsonld = output_dir + "/" + filename_ss_ext + ".jsonld"
 
     jsonld_same_as_existing = parser_function(root + "/" + str(file), CONTEXT_URL,
-                                               output_file=output_jsonld, verbose=verbose)
+                                              output_file=output_jsonld, verbose=verbose)
 
     output_png = output_dir + "/" + filename_ss_ext + ".png"
     if not jsonld_same_as_existing:  # do not generate the png if the jsonld has not evolved
         visualize(output_jsonld, output_file=output_png)
+        if parser_function == afni_to_bids_prov:
+            visualize(output_dir + "/" + filename_ss_ext + "_bloc.jsonld", output_file=output_dir +
+                      "/" + filename_ss_ext + "_bloc.png")
+
 
 def main():
     """
@@ -79,21 +82,24 @@ def main():
 
             if file.endswith("batch.m"):  # spm
                 filename_ss_ext = file.split(".m")[0]
-                process_file(context_write, root, file, filename_ss_ext, output_dir_spm, spm_to_bids_prov, opt.verbose)
+                process_file(context_write, root, file, filename_ss_ext,
+                             output_dir_spm, spm_to_bids_prov, opt.verbose)
 
             elif file.endswith("report_log.html"):  # fsl
                 filename_ss_ext = file.split(".html")[0]
-                process_file(context_write, root, file, filename_ss_ext, output_dir_fsl, fsl_to_bids_prov, opt.verbose)
+                process_file(context_write, root, file, filename_ss_ext,
+                             output_dir_fsl, fsl_to_bids_prov, opt.verbose)
 
             elif file.endswith("proc.sub_001") or file.endswith(".tcsh"):  # afni
                 if ".sub_001" in file:
                     filename_ss_ext = file.split(".sub_001")[0]
                 else:
                     filename_ss_ext = file.split(".tcsh")[0]
-                process_file(context_write, root, file, filename_ss_ext, output_dir_afni, afni_to_bids_prov, opt.verbose)
+                process_file(context_write, root, file, filename_ss_ext,
+                             output_dir_afni, afni_to_bids_prov, opt.verbose)
 
             else:
-                print(" -> Extension of file ", file , " not supported")
+                print(" -> Extension of file ", file, " not supported")
                 continue
 
     context_write.write(f"End of processed files. Results in dir : '{opt.output_dir}'. "
