eebf98e9990a5f1da772e66a83c678ea4a64029d
Boris Clénet <boris.clenet@inria.fr>
2025-06-24 17:18:04 +0200

    Adding code for provenance merge

---
+++ bids_prov/merge.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python
+# coding: utf-8
+
+""" Merge the available provenance records of a BIDS dataset
+    from JSON files into one RDF graph.
+"""
+
+import json
+from argparse import ArgumentParser
+
+from bids import BIDSLayout
+from bids.layout.models import BIDSFile, BIDSJSONFile
+
+def filter_provenance_group(files: list, group: str) -> list:
+    """ Filter a given BIDSFile list, returning the sub-list containig the BIDS
+    `prov` entity equal to group
+    """
+    return [f for f in files if f'prov-{group}' in f.filename]
+
+def get_provenance_files(layout: BIDSLayout, suffix: str, group: str = None) -> list:
+    """ Return a list of provenance files for the dataset. The list is filtered based on
+        a given suffix and a given provenance group.
+    """
+    files = layout.get(suffix=suffix, invalid_filters='allow')
+
+    if group is not None:
+        return filter_provenance_group(files, group)
+    return files
+
+def get_described_datasets(layout: BIDSLayout) -> list:
+    """ Return the dataset_description.json file of a dataset as a BIDSJSONFile. """
+    return [f for f in layout.get() if f.filename == 'dataset_description.json']
+
+def get_described_files(layout: BIDSLayout) -> list:
+    """ Return a list of files of the dataset for which provenance
+        is described inside a sidecar JSON file.
+    """
+    return layout.get(GeneratedBy='', invalid_filters='allow', regex_search=True)
+
+def get_described_sidecars(layout: BIDSLayout) -> list:
+    """ Return a list of files of the dataset for which provenance
+        of sidecar is described inside a sidecar JSON file.
+    """
+    return layout.get(SidecarGeneratedBy='', invalid_filters='allow', regex_search=True)
+
+def get_dataset_entity_record(description_file: BIDSJSONFile) -> dict:
+    """ Return an Entity provenance record from metadata of a BIDS dataset. """
+
+    metadata = description_file.get_dict()
+
+    # Provenance Entity record for the dataset
+    entity = {
+        "Id": "bids:current_dataset",
+        "Label": metadata['Name'],
+        "AtLocation": description_file.dirname,
+        "GeneratedBy": []
+    }
+
+    # Get provenance-related metadata
+    if 'GeneratedBy' in metadata:
+        for generated_by_obj in metadata['GeneratedBy']:
+            if 'Id' in generated_by_obj:
+                entity['GeneratedBy'].append(generated_by_obj['Id'])
+
+    # Return only if GeneratedBy contains something
+    if entity['GeneratedBy']:
+        return entity
+
+    return None
+
+def get_entity_record(data_file: BIDSFile) -> dict:
+    """ Return an Entity provenance record from metadata of a BIDS file. """
+
+    # Provenance Entity record for the data file
+    entity = {
+        "Id": f"bids::{data_file.relpath}",
+        "Label": data_file.filename,
+        "AtLocation": data_file.path
+    }
+
+    # Get provenance-related metadata
+    metadata = data_file.get_metadata()
+    if 'GeneratedBy' in metadata:
+        entity['GeneratedBy'] = metadata['GeneratedBy']
+    if 'Digest' in metadata:
+        entity['Digest'] = metadata['Digest']
+    if 'Type' in metadata:
+        entity['Type'] = metadata['Type']
+
+    return entity
+
+def get_sidecar_entity_record(data_file: BIDSJSONFile) -> dict:
+    """ Return an Entity provenance record for the sidecar of a BIDS file, given its metadata. """
+
+    # Get sidecar associated with the data_file
+    sidecar = data_file.get_associations()[0]
+
+    # Provenance Entity record for the sidecar JSON file
+    entity = {
+        "Id": f"bids::{sidecar.relpath}",
+        "Label": sidecar.filename,
+        "AtLocation": sidecar.path
+    }
+
+    # Get provenance-related metadata
+    metadata = data_file.get_metadata()
+    if 'SidecarGeneratedBy' in metadata:
+        entity['GeneratedBy'] = metadata['SidecarGeneratedBy']
+        return entity
+
+    return None
+
+def entry_point():
+    """ A command line tool for the merge module """
+
+    parser = ArgumentParser()
+    parser.add_argument('--dataset', '-d', type=str, default='.',
+        help='The path to the input BIDS dataset.')
+    parser.add_argument('--output_file', '-o', type=str, required=True,
+        help='Output JSON-LD file containing the provenance graph for the input dataset.')
+    parser.add_argument('--group', '-g', type=str,
+        help='Provenance group for which to extract the metadata.')
+    arguments = parser.parse_args()
+
+    # Base for the output JSON-LD
+    base_provenance = {
+      "BIDSProvVersion": "0.0.1",
+      "@context": "https://purl.org/nidash/bidsprov/context.json",
+      "Records": {
+        "Software": [],
+        "Activities": [],
+        "Entities": []
+      }
+    }
+
+    # Input dataset
+    layout = BIDSLayout(arguments.dataset)
+
+    # Get provenance metadata form provenance files
+    for file in get_provenance_files(layout, suffix='ent', group=arguments.group):
+        base_provenance['Records']['Entities'] += file.get_dict()['Entities']
+    # TODO : add environment feature
+    #for file in get_provenance_files(layout, suffix='env', group=arguments.group):
+    #    base_provenance['Records']['Environments'] += file.get_dict()['Environments']
+    for file in get_provenance_files(layout, suffix='act', group=arguments.group):
+        base_provenance['Records']['Activities'] += file.get_dict()['Activities']
+    for file in get_provenance_files(layout, suffix='soft', group=arguments.group):
+        base_provenance['Records']['Software'] += file.get_dict()['Software']
+
+    # Get provenance metadata from other JSON files in the dataset
+    # TODO : how to filter on provenance group ?
+    for data_file in get_described_files(layout):
+        entity = get_entity_record(data_file)
+        base_provenance['Records']['Entities'].append(entity)
+    for sidecar in get_described_sidecars(layout):
+        entity = get_sidecar_entity_record(sidecar)
+        if entity is not None:
+            base_provenance['Records']['Entities'].append(entity)
+    for dataset in get_described_datasets(layout):
+        entity = get_dataset_entity_record(dataset)
+        if entity is not None:
+            base_provenance['Records']['Entities'].append(entity)
+
+    # Write output JSON-LD file
+    with open(arguments.output_file, 'w', encoding = 'utf-8') as file:
+        file.write(json.dumps(base_provenance, indent = 2))
+
+if __name__ == '__main__':
+    entry_point()
